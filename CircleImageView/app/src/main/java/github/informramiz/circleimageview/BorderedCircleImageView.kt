a774c3b5cda6ae16da885b8b225b2d034a610a4f
Ramiz Raja <informramiz@gmail.com>
2020-07-29 16:31:08 +0500

    feat: Complete BorderedCircleImageView

--- CircleImageView/app/src/main/java/github/informramiz/circleimageview/BorderedCircleImageView.kt
+++ CircleImageView/app/src/main/java/github/informramiz/circleimageview/BorderedCircleImageView.kt
@@ -1,6 +1,7 @@
 package github.informramiz.circleimageview
 
 import android.content.Context
+import android.graphics.Color
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
@@ -8,7 +9,9 @@ import android.view.ViewGroup
 import android.view.ViewOutlineProvider
 import android.widget.FrameLayout
 import androidx.core.content.ContextCompat
+import androidx.core.content.withStyledAttributes
 import androidx.core.view.setMargins
+import androidx.core.view.setPadding
 
 
 /**
@@ -17,16 +20,36 @@ import androidx.core.view.setMargins
 class BorderedCircleImageView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
+
+    private var borderSize = 0
+    private var borderColor: Int = Color.GRAY
+    private var imageDrawable: Drawable? = null
+    private var circleImageView: CircleImageView
+
     init {
         //the outline (view edges) of the view should be derived from the background
         outlineProvider = ViewOutlineProvider.BACKGROUND
         //cut the view to match the view to the outline of the background
         clipToOutline = true
+
+        //getting the custom attributes from the XML declaration
+        context.withStyledAttributes(attrs, R.styleable.BorderedCircleImageView) {
+            borderSize = getDimensionPixelSize(R.styleable.BorderedCircleImageView_bciv_border_size, 0)
+            borderColor = getColor(R.styleable.BorderedCircleImageView_bciv_border_color, borderColor)
+            imageDrawable = getDrawable(R.styleable.BorderedCircleImageView_bciv_src)
+        }
+
         //use the following background to calculate the outline
         background = getBackgroundDrawable()
+        //to achieve border we will set padding so that the background drawable is visible
+        //from the edges and acts as a border
+        setPadding(borderSize)
 
         //add the circle image as the child view
-        addView(getCircleImageView())
+        circleImageView = getCircleImageView()
+        //set the passed image src to the CircleImageView
+        imageDrawable?.let { circleImageView.setImageDrawable(it) }
+        addView(circleImageView)
     }
 
     private fun getBackgroundDrawable(): Drawable {
@@ -35,12 +58,9 @@ class BorderedCircleImageView @JvmOverloads constructor(
         // change it on all other screens which is not what we want. That's why we call mutate()
         // so that a mutable copy is created
         val drawable = context.getDrawable(R.drawable.bg_circle)!!.mutate()
-        (drawable as GradientDrawable).setColor(
-            ContextCompat.getColor(
-                context,
-                android.R.color.darker_gray
-            )
-        )
+        //these XML shapes are all GradientDrawables as they lets you set fill, stroke
+        // and gradient colors so we can safely cast this drawable to GradientDrawable
+        (drawable as GradientDrawable).setColor(borderColor)
         return drawable
     }
 

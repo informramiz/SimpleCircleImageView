9a1be573ddc21eae94144551e618a99de91748cc
Ramiz Raja <informramiz@gmail.com>
2020-10-10 19:00:18 +0500

    refactor: Use viewBinding for layouts

--- CircleImageView/app/src/main/java/github/informramiz/circleimageview/MainActivity.kt
+++ CircleImageView/app/src/main/java/github/informramiz/circleimageview/MainActivity.kt
@@ -2,10 +2,12 @@ package github.informramiz.circleimageview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import github.informramiz.circleimageview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val viewBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewBinding.root)
     }
 }
--- CircleImageView/app/src/main/java/github/informramiz/circleimageview/MaterialActivity.kt
+++ CircleImageView/app/src/main/java/github/informramiz/circleimageview/MaterialActivity.kt
@@ -2,10 +2,12 @@ package github.informramiz.circleimageview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import github.informramiz.circleimageview.databinding.ActivityMaterialBinding
 
 class MaterialActivity : AppCompatActivity() {
+    private val viewBinding by lazy { ActivityMaterialBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_material)
+        setContentView(viewBinding.root)
     }
 }
